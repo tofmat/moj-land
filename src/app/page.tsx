424a38663bc01d643ae3e192b534ab808c0a89e2
Tosin Ogunfowote <tofmatt@gmail.com>
2024-02-16 22:47:06 +0100

    sample icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Additem, Airpods } from "iconsax-react";
+
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      Hello
+      <div>
+        Sample of icon
+        <Additem size="32" color="#FF8A65" />
+        <Airpods size="32" color="#FF8A65" variant="Bold" />
+      </div>
+    </main>
+  );
 }
